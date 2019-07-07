0c27cc4551b9039faabb394c1ca4bed6c5f2d3ec
vadim_sapovsky <v.sapovskiy@gmail.com>
2019-07-07 13:08:20 +0300

    Added Express

--- app/index.js
+++ app/index.js
@@ -1,10 +1,27 @@
-const calc = require('./calc');
-const numbersToAdd = [
-    3,
-    4,
-    10,
-    2
-];
+const path = require('path');
+const express = require('express');
+const exphbs = require('express-handlebars');
+const app = express();
+const port = 3000;
 
-const result = calc.sum(numbersToAdd);
-console.log(`The result is: ${result}`);
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    extname: '.hbs',
+    layoutsDir: path.join(__dirname, 'views/layouts');
+}));
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (request, response) => {
+    response.render('home', {
+        name: 'Evgeniya'
+    });
+});
+
+app.listen(port, (err) => {
+    if (err) {
+        return console.log('something bad happened', err);
+    }
+
+    console.log(`server is listening on ${port}`);
+});
